c0757ba6cb8ed4cb51da22554ebce6ec7e614ecb
1nto5 <aantosiak@icloud.com>
2025-05-14 06:51:06 +0200

    refactor: reorganize cron job scheduling for clarity and remove redundant tasks

--- index.js
+++ index.js
@@ -9,30 +9,26 @@ import { syncR2platnikEmployees } from './sync-r2platnik-employees.js';
 
 dotenv.config();
 
-// Schedule synchronization of employees at 06:00, 14:00, and 22:00 every day
-cron.schedule('0 6,14,22 * * *', syncR2platnikEmployees);
+// Deviations tasks
+// -----------------------
+// Schedule sending of pending deviation approval notifications every workday at 03:00
+cron.schedule('0 3 * * 1-5', sendPendingDeviationApprovalNotifications, {});
+// Schedule deviations status update every 2 hours
+cron.schedule('0 */2 * * *', deviationsStatusUpdate, {});
 
+// Production overtime tasks
+// -------------------------------
+// Schedule sending of pending production overtime email notifications every workday at 3:00
+cron.schedule('0 3 * * 1-5', sendPendingProductionOvertimeEmailNotifications);
+
+// Data synchronization tasks
+// --------------------------
+// Schedule synchronization of r2platnik employees at 16:00 every workday
+cron.schedule('0 16 * * 1-5', syncR2platnikEmployees);
+// Schedule synchronization of LDAP users every workday at 16:00
+cron.schedule('0 16 * * 1-5', syncLdapUsers);
+
+// Maintenance tasks
+// ----------------
 // Schedule archiving of scans every Sunday at 22:00
 cron.schedule('0 22 * * 0', archiveScans);
-
-// Schedule sending of pending production overtime email notifications every day at 22:00
-cron.schedule('0 22 * * *', sendPendingProductionOvertimeEmailNotifications);
-
-// For testing: Schedule email sending every minute
-// cron.schedule('* * * * *', sendPendingProductionOvertimeEmailNotifications, {});
-
-// Schedule synchronization of LDAP users every day at 16:00
-cron.schedule('0 16 * * *', syncLdapUsers);
-
-// Schedule synchronization of LDAP users every minute for testing
-// cron.schedule('* * * * *', syncLdapUsers, {});
-
-// Schedule deviations status update every day at 03:00
-cron.schedule('0 3 * * *', deviationsStatusUpdate, {});
-// Schedule deviations status update every minute for testing
-// cron.schedule('* * * * *', deviationsStatusUpdate, {});
-
-// Schedule sending of pending deviation approval notifications every day at 03:00
-cron.schedule('0 3 * * *', sendPendingDeviationApprovalNotifications, {});
-// Schedule sending of pending deviation approval notifications every minute for testing
-// cron.schedule('* * * * *', sendPendingDeviationApprovalNotifications, {});
